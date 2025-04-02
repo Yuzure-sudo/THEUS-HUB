06b0d57f8dc84b0da437867f5cc9e012e57aa892
Yuzure <lipehhgowner@gmail.com>
2025-04-02 18:18:52 -0300

    Criar loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local TheusHub = {}
+
+TheusHub.Load = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/THEUS-HUB/main/source.lua"))()
+end
+
+return TheusHub.Load()
