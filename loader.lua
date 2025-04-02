c91dbe7f303ce0e935f5b1af4d797b3556fdcb4f
Yuzure <lipehhgowner@gmail.com>
2025-04-02 18:22:34 -0300

    Atualizar o loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,9 @@
-local TheusHub = {}
-
-TheusHub.Load = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/THEUS-HUB/main/source.lua"))()
+local function LoadTheusHub()
+    if game.PlaceId == 4520749081 then -- King Legacy
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/THEUS-HUB/main/source.lua"))()
+    else
+        game.Players.LocalPlayer:Kick("THEUS HUB: Jogo não suportado!")
+    end
 end
 
-return TheusHub.Load()
+return LoadTheusHub()
